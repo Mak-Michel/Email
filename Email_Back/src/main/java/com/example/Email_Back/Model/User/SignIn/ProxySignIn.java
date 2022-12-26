c0c7b8d281d9cddd18a7198a77484f5f30abe817
Random <ehabflash833@gmail.com>
2022-12-26 19:41:30 +0200

    handle wrong user inputs

--- Email_Back/src/main/java/com/example/Email_Back/Model/User/SignIn/ProxySignIn.java
+++ Email_Back/src/main/java/com/example/Email_Back/Model/User/SignIn/ProxySignIn.java
@@ -7,10 +7,12 @@ public class ProxySignIn implements ISignIn{
 
     private ISignIn user;
     private String email;
+    private String mailExtension;
     private String password;
 
     public ProxySignIn(String name, String email, String password) {
         this.setEmail(email.substring(0, email.indexOf("@")));
+        this.mailExtension = email.substring(email.indexOf("@"));
         this.setPassword(password);
     }
 
@@ -31,22 +33,30 @@ public class ProxySignIn implements ISignIn{
     }
 
     public User loadUser() {
+        this.checkValidInputs();
         this.checkExistence();
         this.checkPassword();
         this.user = new SignIn(this.email, this.password);
         return this.user.loadUser();
     }
 
+    private void checkValidInputs() {
+        if(!this.mailExtension.equals("@mymail.com"))
+            throw new RuntimeException("Email is written in a wrong format!");
+        if(this.password.equals(""))
+            throw new RuntimeException("Password cannot be empty!");
+    }
+
     private void checkExistence() {
         UserHandler check = new UserHandler(this.email);
         if(!check.exists())
-            throw new RuntimeException("User is not found");
+            throw new RuntimeException("User is not found!");
     }
 
     private void checkPassword() {
         UserHandler check = new UserHandler(this.email);
         if(!check.rightPassword(this.password))
-            throw new RuntimeException("Password is incorrect");
+            throw new RuntimeException("Password is incorrect!");
     }
 
 }
--- Email_Back/src/main/java/com/example/Email_Back/Model/User/SignUp/ProxySignUp.java
+++ Email_Back/src/main/java/com/example/Email_Back/Model/User/SignUp/ProxySignUp.java
@@ -7,11 +7,13 @@ public class ProxySignUp implements ISignUp{
     private ISignUp user;
     private String name;
     private String email;
+    private String mailExtension;
     private String password;
 
     public ProxySignUp(String name, String email, String password) {
         this.setName(name);
         this.setEmail(email.substring(0, email.indexOf("@")));
+        this.mailExtension = email.substring(email.indexOf("@"));
         this.setPassword(password);
     }
 
@@ -40,9 +42,19 @@ public class ProxySignUp implements ISignUp{
     }
 
     public void addUser() {
+        this.checkValidInputs();
         this.checkExistence();
         this.user = new SignUp(this.name, this.email, this.password);
         this.user.addUser();
+    }
+
+    private void checkValidInputs() {
+        if(!this.mailExtension.equals("@mymail.com"))
+            throw new RuntimeException("Email is written in a wrong format!");
+        if(this.name.equals(""))
+            throw new RuntimeException("Name cannot be empty!");
+        if(this.password.equals(""))
+            throw new RuntimeException("Password cannot be empty!");
     }
 
     private void checkExistence() {
