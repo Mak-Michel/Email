9622fa65a1cae79af0fb1c9d5dd01fcc14d1a50b
Random <ehabflash833@gmail.com>
2022-12-31 18:47:09 +0200

    Update proxy.service.ts

--- Email_Front/src/app/Controller/Proxy/proxy.service.ts
+++ Email_Front/src/app/Controller/Proxy/proxy.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Email } from '../Classes/Email';
-import { User } from '../Classes/user';
+import { User } from '../Classes/User';
 import { HttpService } from '../Http/http.service';
 
 @Injectable({
